f8db838ea18a9298fd7a40889d3fe52fc31f44d1
Adriano Tanoue <adriano.tanoue@gmail.com>
2019-10-02 22:16:56 +0200

    using express-generator-typescript as boiler plate

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import * as _ from './server/env';
-import { serve } from './server';
-// import { connectDb } from './models';
-
-// connectDb().then(async () => {
-//   await app.server;
-// });
-serve;
